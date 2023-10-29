1abb3df79607382491a6ce3e767a40e5e4c1ff17
stum0 <70952638+stum0@users.noreply.github.com>
2023-10-29 23:29:42 +0000

    add home button

--- src/gui_plugin.rs
+++ src/gui_plugin.rs
@@ -549,7 +549,7 @@ fn update_text(
 
         new_text_value = match send_net_msg.player_type {
             3 => "Spectating",
-            _ if board.player_turn == send_net_msg.player_type => "Its Your Turn",
+            _ if board.player_turn == send_net_msg.player_type => "Its your Turn",
             _ => "Waiting for Player 2...",
         };
     }
